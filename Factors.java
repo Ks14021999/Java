31983f500062ed4e094b2b14d1a9d4b1b327a47d
Krishan <ks14021999@gmail.com>
2022-06-01 20:07:42 +0530

    V 1.0

---
+++ Factors.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        for(int i = 2; i < N; i ++){
+            if(N % i == 0){
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+}
