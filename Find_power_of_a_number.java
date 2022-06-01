26475557a1800b91b4b83c9bcc9db503126e158c
Krishan <ks14021999@gmail.com>
2022-06-01 20:19:05 +0530

    V 1.0

---
+++ Find_power_of_a_number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Find_power_of_a_number {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int n = s.nextInt();
+        int ans = 1;
+        while(n > 0){
+            ans *= x;
+            n--;
+        }
+        System.out.print(ans);
+    }
+}
