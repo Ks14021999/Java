30fee54695a21846183b32a3c8229f4aa7e9435f
Krishan <ks14021999@gmail.com>
2022-06-01 20:02:00 +0530

    V 1.0

---
+++ Sum_of_even_and_odd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Sum_of_even_and_odd {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        int even = 0;
+        int odd = 0;
+        while(N != 0){
+            if(N%2 == 0){
+                even += N%10;
+            }
+            else{
+                odd += N%10;
+            }
+            N /=10;
+        }
+        System.out.print(even +" " + odd);
+    }
+}
