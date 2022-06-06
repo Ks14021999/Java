d4c0d7c0660fe9c01f9e4d53dbcafe2b4f5fe15e
Krishan <ks14021999@gmail.com>
2022-06-07 03:41:51 +0530

    V 1.0

---
+++ Patterns_2/Parallelogram_Pattern.java
@@ -0,0 +1,25 @@
+package Patterns_2;
+
+import java.util.Scanner;
+
+public class Parallelogram_Pattern {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        int i = 0;
+        while(i < N){
+            int j = 0;
+            int space = 0;
+            while(space < i){
+                System.out.print(" ");
+                space++;                
+            }
+            while(j < N){
+                System.out.print("*");
+                j++;
+            }
+            System.out.println("");
+            i++;
+        }
+    }
+}
