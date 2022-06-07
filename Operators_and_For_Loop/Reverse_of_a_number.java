13ef6f0e9bc3d94d72945a798cf1ab05fd1f49ad
Krishan <ks14021999@gmail.com>
2022-06-07 19:11:54 +0530

    V 1.0

--- Operators_and_For_Loop/Reverse_of_a_number.java
+++ Operators_and_For_Loop/Reverse_of_a_number.java
@@ -8,11 +8,9 @@ public class Reverse_of_a_number {
         Scanner s = new Scanner(System.in);
         int N = s.nextInt();
         int RN = 0;
-        int i = 1;
-        while(N == 0){
+        while(N != 0){
             int temp = N % 10;
-            RN = (RN * i) + temp; 
-            i *= 10;
+            RN = (RN * 10) + temp;
             N /= 10;
         }
         System.out.print(RN);
