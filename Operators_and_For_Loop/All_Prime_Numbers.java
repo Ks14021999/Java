32277b5470059c6f2c069ec5a3bdbf3daae4d4a4
Krishan <ks14021999@gmail.com>
2022-06-07 18:38:44 +0530

    V 1.0

--- Operators_and_For_Loop/All_Prime_Numbers.java
+++ Operators_and_For_Loop/All_Prime_Numbers.java
@@ -18,5 +18,6 @@ public class All_Prime_Numbers {
                 System.out.println(i);
             }
         }
+        s.close();
     }
 }
--- Operators_and_For_Loop/Sum_or_Product.java
+++ Operators_and_For_Loop/Sum_or_Product.java
@@ -1,5 +1,29 @@
 package Operators_and_For_Loop;
 
+import java.util.Scanner;
+
 public class Sum_or_Product {
-    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        int C = s.nextInt();
+        if(C == 1){
+            int Sum = 0;
+            for(int i = 1; i <= N; i++){
+                Sum += i;
+            }
+            System.out.print(Sum);
+        }
+        else if(C == 2){
+            int product = 1;
+            for(int i = 1; i <= N; i++){
+                product *= i;
+            }
+            System.out.print(product);
+        }
+        else{
+            System.out.print(-1);
+        }
+        s.close();
+    }
 }
