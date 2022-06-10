e48ef14cf01345f9c47ab4de5ade36a67b35abc4
Krishan <ks14021999@gmail.com>
2022-06-10 18:48:36 +0530

    V 1.0

---
+++ Operators_and_For_Loop/Check_Number_sequence.java
@@ -0,0 +1,27 @@
+package Operators_and_For_Loop;
+
+import java.util.Scanner;
+
+public class Check_Number_sequence {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int seq = 1;
+        int d = b - a;
+        for(int i =3; i <=N; i++){
+            a = b;
+            b = s.nextInt();
+            if(b- a != d){
+                seq = 0;
+            }
+        }
+        if(seq == 0){
+            System.out.println("false");
+        }
+        else{
+            System.out.println("true");
+        }
+    }
+}
